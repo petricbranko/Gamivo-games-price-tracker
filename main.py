9f3b483f837b9914113fa000e360ba1f2af4a43c
petricbranko <brankopetric2000@gmail.com>
2020-06-15 00:37:35 +0200

    added database

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import requests
+import sqlite3
 from bs4 import BeautifulSoup
+
+conn = sqlite3.connect('games.db')
+cur = conn.cursor()
+
+cur.execute('''CREATE TABLE IF NOT EXISTS Games(
+    Name TEXT PRIMARY KEY,
+    Price REAL
+) ''')
 
 links = [
     "https://www.gamivo.com/product/grand-theft-auto-v-gta-5",
@@ -14,6 +23,21 @@ for link in links:
     page = requests.get(link, headers=headers)
     soup = BeautifulSoup(page.content, 'html.parser')
     price = soup.find("div", {"class": "price lowest-price"}).get_text()
+    converted_price = float(price[2:])
     title = soup.title.string[3:]
 
     print("Name: " + title + " | Price: " + '\033[91m' + price + '\033[0m')
+    try:
+        cur.execute('''INSERT INTO Games 
+                    (Name, Price) VALUES(?, ?)''', 
+                    (title, converted_price))
+        conn.commit()
+    except sqlite3.IntegrityError:
+        cur.execute('''UPDATE Games
+                       SET 
+                        Price = (?)
+                       WHERE 
+                        Name = (?) ''',
+                        (converted_price,title)
+        )
+        conn.commit()
